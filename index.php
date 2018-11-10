844f9b98d13a5fcdb00331405357b19bb18bb143
cpeter <cpeter@126.com>
2018-11-10 23:30:26 +0800

    /

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ define('BASEDIR',__DIR__);
 include BASEDIR.'/IMooc/Loader.php';
 spl_autoload_register('\\Imooc\\Loader::autoload');
 IMooc\Object::test();
-App\Controller\Home\Index::test();
+App\Controller\Home\Index::test();;
